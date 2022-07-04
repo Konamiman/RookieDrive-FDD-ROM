8ead78b1bf37208a6c0387168af41a7517edd66b
Nestor Soriano <konamiman@konamiman.com>
2022-07-04 16:54:54 +0200

    Fix the build.sh script

--- msx/build.sh
+++ msx/build.sh
@@ -9,31 +9,35 @@ echo
 
 sjasm $SRC_PATH/rookiefdd.asm $BIN_PATH/rookiefdd${VERSION}_normal.rom
 
+cp $SRC_PATH/rookiefdd.asm $SRC_PATH/rookiefdd.bak 
+sed -i 's/"config.asm"/"temp.asm"/g' $SRC_PATH/rookiefdd.asm
+
 sed 's/INVERT_CTRL_KEY: equ 0/INVERT_CTRL_KEY: equ 1/g' $SRC_PATH/config.asm > $SRC_PATH/temp.asm
-sjasm $SRC_PATH/temp.asm $BIN_PATH/rookiefdd${VERSION}_inverted_ctrl.rom
+sjasm $SRC_PATH/rookiefdd.asm $BIN_PATH/rookiefdd${VERSION}_inverted_ctrl.rom
 
 sed 's/DISABLE_OTHERS_BY_DEFAULT: equ 0/DISABLE_OTHERS_BY_DEFAULT: equ 1/g' $SRC_PATH/config.asm > $SRC_PATH/temp.asm
-sjasm $SRC_PATH/temp.asm $BIN_PATH/rookiefdd${VERSION}_exclusive.rom
+sjasm $SRC_PATH/rookiefdd.asm $BIN_PATH/rookiefdd${VERSION}_exclusive.rom
 
 sed 's/USE_ALTERNATIVE_PORTS: equ 0/USE_ALTERNATIVE_PORTS: equ 1/g' $SRC_PATH/config.asm > $SRC_PATH/temp.asm
-sjasm $SRC_PATH/temp.asm $BIN_PATH/rookiefdd${VERSION}_alt_ports.rom
+sjasm $SRC_PATH/rookiefdd.asm $BIN_PATH/rookiefdd${VERSION}_alt_ports.rom
 
 sed 's/INVERT_CTRL_KEY: equ 0/INVERT_CTRL_KEY: equ 1/g' $SRC_PATH/config.asm > $SRC_PATH/temp.asm
 sed -i 's/DISABLE_OTHERS_BY_DEFAULT: equ 0/DISABLE_OTHERS_BY_DEFAULT: equ 1/g' $SRC_PATH/temp.asm
-sjasm $SRC_PATH/temp.asm $BIN_PATH/rookiefdd${VERSION}_exclusive_inverted_ctrl.rom
+sjasm $SRC_PATH/rookiefdd.asm $BIN_PATH/rookiefdd${VERSION}_exclusive_inverted_ctrl.rom
 
 sed 's/INVERT_CTRL_KEY: equ 0/INVERT_CTRL_KEY: equ 1/g' $SRC_PATH/config.asm > $SRC_PATH/temp.asm
 sed -i 's/USE_ALTERNATIVE_PORTS: equ 0/USE_ALTERNATIVE_PORTS: equ 1/g' $SRC_PATH/temp.asm
-sjasm $SRC_PATH/temp.asm $BIN_PATH/rookiefdd${VERSION}_alt_ports_inverted_ctrl.rom
+sjasm $SRC_PATH/rookiefdd.asm $BIN_PATH/rookiefdd${VERSION}_alt_ports_inverted_ctrl.rom
 
 sed 's/DISABLE_OTHERS_BY_DEFAULT: equ 0/DISABLE_OTHERS_BY_DEFAULT: equ 1/g' $SRC_PATH/config.asm > $SRC_PATH/temp.asm
 sed -i 's/USE_ALTERNATIVE_PORTS: equ 0/USE_ALTERNATIVE_PORTS: equ 1/g' $SRC_PATH/temp.asm
-sjasm $SRC_PATH/temp.asm $BIN_PATH/rookiefdd${VERSION}_alt_ports_exclusive.rom
+sjasm $SRC_PATH/rookiefdd.asm $BIN_PATH/rookiefdd${VERSION}_alt_ports_exclusive.rom
 
 sed 's/INVERT_CTRL_KEY: equ 0/INVERT_CTRL_KEY: equ 1/g' $SRC_PATH/config.asm > $SRC_PATH/temp.asm
 sed -i 's/DISABLE_OTHERS_BY_DEFAULT: equ 0/DISABLE_OTHERS_BY_DEFAULT: equ 1/g' $SRC_PATH/temp.asm
 sed -i 's/USE_ALTERNATIVE_PORTS: equ 0/USE_ALTERNATIVE_PORTS: equ 1/g' $SRC_PATH/temp.asm
-sjasm $SRC_PATH/temp.asm $BIN_PATH/rookiefdd${VERSION}_alt_ports_exclusive_inverted_ctrl.rom
+sjasm $SRC_PATH/rookiefdd.asm $BIN_PATH/rookiefdd${VERSION}_alt_ports_exclusive_inverted_ctrl.rom
 
+cp $SRC_PATH/rookiefdd.bak $SRC_PATH/rookiefdd.asm
+rm $SRC_PATH/rookiefdd.bak
 rm $SRC_PATH/temp.*
-
